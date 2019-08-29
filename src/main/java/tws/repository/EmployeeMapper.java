521290243894ff7a3fad71472733f6f2c16b89d3
zhuchenchen <550400438@qq.com>
2019-08-29 16:39:50 +0800

    test

--- src/main/java/tws/repository/EmployeeMapper.java
+++ src/main/java/tws/repository/EmployeeMapper.java
@@ -1,11 +1,22 @@
 package tws.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import tws.entity.Employee;
 
 import java.util.List;
 
 @Mapper
 public interface EmployeeMapper {
-    List<Employee> selectAll();
+ 
+ int insertEmployee(@Param("employee") Employee employee);
+
+ List<Employee> getEmployeeList();
+ 
+ Employee getEmployeeById(@Param("id") int id);
+ 
+ int updateEmployee(@Param("id") int id, @Param("employee") Employee employee);
+ 
+ int deleteEmployee(@Param("id") int id);
 }
